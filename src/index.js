1dd5912c3301012537f2097f6b638146faa84f19
Ashley-Zhao <ashley.zhao@ibm.com>
2022-11-20 21:20:34 -0500

    Removed extraneous files

--- src/index.js
+++ src/index.js
@@ -151,12 +151,8 @@ function Dot(props) {
 
 	const ref = useRef();
 
-	const roundedSquareWave = (t, delta, a, f) => {
-		return ((2 * a) / Math.PI) * Math.atan(Math.sin(2 * Math.PI * t * f) / delta)
-	  }
-
 	const { vec, transform, positions, distances } = useMemo(() => {
-		const vec = new THREE.Vector3()
+		const vec = new THREE.Vector2()
 		const transform = new THREE.Matrix4()
 	
 		// Precompute randomized initial positions
@@ -168,10 +164,6 @@ function Dot(props) {
 	
 		  // Offset every other column (hexagonal pattern)
 		  position.y += (i % 2) * 0.5
-	
-		  // Add some noise
-		  position.x += Math.random() * 0.3
-		  position.y += Math.random() * 0.3
 		  return position
 		})
 	
@@ -182,24 +174,21 @@ function Dot(props) {
 		})
 		return { vec, transform, positions, distances }
 	  }, [])
-	  useFrame(({ clock }) => {
+
+	  useFrame(({ }) => {
 		for (let i = 0; i < 10000; ++i) {
-		  const dist = distances[i]
-	
-		  // Distance affects the wave phase
-		  const t = clock.elapsedTime - dist / 25
-	
-		  // Oscillates between -0.4 and +0.4
-		  const wave = roundedSquareWave(t, 0.15 + (0.2 * dist) / 72, 0.4, 1 / 3.8)
-	
-		  // Scale initial position by our oscillator
-		  vec.copy(positions[i]).multiplyScalar(wave + 1.3)
+
+		  /* // Scale initial position by our oscillator
+		  vec.copy(positions[i]).multiplyScalar(0 + 1.3)
 	
 		  // Apply the Vector3 to a Matrix4
 		  transform.setPosition(vec)
 	
 		  // Update Matrix4 for this instance
+		  ref.current.setMatrixAt(i, transform) */
+
 		  ref.current.setMatrixAt(i, transform)
+		
 		}
 		ref.current.instanceMatrix.needsUpdate = true
 	  })
@@ -207,7 +196,7 @@ function Dot(props) {
 	return (
 	  <instancedMesh ref={ref} args={[null, null, 10000]}>
 		<circleBufferGeometry attach="geometry" args={[0.05, 100]} />      
-		<meshNormalMaterial attach="material" />
+		<meshNormalMaterial attach="material" color="white"/>
 	  </instancedMesh>
 	);
   }
